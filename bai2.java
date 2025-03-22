70a15608e70a1bac9d3858fe59304b3736fa9d66
luyeen-lee <luyenlt@utop.io>
2025-03-22 18:37:16 +0700

    bai2

---
+++ bai2.java
@@ -0,0 +1,20 @@
+public class MyThread extends Thread {
+    int iNumber = 12345;
+
+    public void methodOne() {
+        Thread t = new Thread(() {
+            System.out.println( iNumber);
+        });
+        t.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread obj = new MyThread();
+        obj.methodOne();
+    }
+}
